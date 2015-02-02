d619bbcc8ed8eef91d186d936b66df05bd3c7518
Nik Butenko <nik@butenko.me>
2015-02-02 23:47:06 +1100

    Get current branch from git

--- commands/start.js
+++ commands/start.js
@@ -4,10 +4,7 @@ var git = require('../lib/git');
 var start = function (key) {
   var name = summary(key).shift();
   var result = [];
-  var branch = [
-    'feature',
-    [key, name].join(' ').replace(/[^a-z_0-9]+/ig, '-')
-  ].join('/');
+  var branch = git.featureBranchName([key, name].join(' '));
 
 
   // create branch if does not exist
