624414913efa44716c04e5173e3b4fdb4e3aacd9
Nik Butenko <nik@butenko.me>
2015-02-02 20:22:15 +1100

    Help and usage for all commands

--- index.js
+++ index.js
@@ -2,29 +2,40 @@
 
 var path = require('path');
 var glob = require('glob');
+var _ = require('lodash');
 
 var args = process.argv.slice(2);
 var command = args.shift();
 
+// Pre-fill all commands to populate help
 var commands = {};
 glob.sync(path.join('lib', '*.js')).forEach(function (filename) {
   commands[path.basename(filename, '.js')] = require(path.resolve(filename));
 });
 
 
+function usage() {
+  console.log(['Usage:'].concat(_.map(commands, function(handler, name) {
+    return [name + ':'].concat(handler.help).join('\n    ')
+  })).join('\n  '));
+}
+
+if (!command || command === 'help') {
+  usage();
+  process.exit(0);
+}
+
 if (!commands[command]) {
-  return console.error('Command `' + command + '` not implemented');
+  console.error('Command `' + command + '` not implemented');
+  usage();
+  process.exit(1);
 }
 
 try {
-  console.log('OK' +
-    [''].concat(commands[command].apply(null, args)).join('\n    ')
-  );
+  console.log(['OK'].concat(commands[command].apply(null, args)).join('\n    '));
 } catch (e) {
   console.error(e);
-  console.log('Usage:' +
-    [''].concat(commands[command].help).join('\n    ')
-  );
+  console.log(['Usage'].concat(commands[command].help).join('\n    '));
   process.exit(1);
 }
 
